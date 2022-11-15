5f8c8e6264fe9c5bf79af3df6c3d9f55e9d2c3a9
Ritambra yadav <ritambrayadav20@gmail.com>
2022-11-15 23:32:15 +0530

    added category section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,23 +3,62 @@ import './App.css';
 import './component/style.css'
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
-import { responsive, productData } from './component/data';
+import { responsive,productData, responsive_1,responsive_2, productData_1, productData_2 } from './component/data';
 import Header from './component/Header';
 import Firstsec from './component/Firstsec';
+import Secondsec from './component/Secondsec';
+import Categ from './component/Categ';
 
 function App() {
   const product = productData.map((item)=>(
+    <Categ
+    cat ={item.cat} />
+  ))
+
+  const product_1 = productData_1.map((item)=>(
     <Firstsec
     heading={item.heading}
     url={item.imageurl}
-    price={item.price}
     description={item.description} />
+  ))
+
+  const product_2 = productData_2.map((item)=>(
+    <Secondsec
+    url={item.imageurl}
+    location={item.location}
+    description={item.description} 
+    price={item.price} />
   ))
   return (
     <div className="App">
+      <Header />
+      
+      <div>
+        <h2 style={{color:"black",textAlign:"left"}}>New this week</h2>
+      <Carousel responsive={responsive_1}>
+        {product_1}
+      </Carousel>
+      </div>
+
+      <div>
+        <div style={{display:"flex"}}>
+      <div>Dates</div>
+      <div>Group size</div>
+      <div>More filters</div>
+      <div style={{width:"1px",height:"20px",background:"grey"}}></div>
+      </div>
       <Carousel responsive={responsive}>
         {product}
       </Carousel>
+      </div>
+
+      <div>
+      <h3 style={{color:"black",textAlign:"left"}}>Top sellers</h3>
+      <Carousel responsive={responsive_2}>
+        {product_2}
+      </Carousel>
+      </div>
+
     </div>
   );
 }
--- my-app/src/component/Firstsec.js
+++ my-app/src/component/Firstsec.js
@@ -5,7 +5,7 @@ function Firstsec(props) {
         <div className="card">
       <img className="product_image" src={props.url} alt="product image" />
       <p>
-      <h4>Collection</h4>
+      <h6>Collection</h6>
       <h2>{props.heading}</h2>
       </p>
         <button>Show all</button>
