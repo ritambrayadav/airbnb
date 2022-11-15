c7b3682ae3cae4b9d74e682582b52cfd89d6fd0b
Ritambra yadav <ritambrayadav20@gmail.com>
2022-11-15 11:37:53 +0530

    started working on first slide section

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { responsive,  } from './component/data';
+import Header from './component/Header';
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>React multi carousel</h2>
+      <Carousel responsive={responsive}>
+        
+      </Carousel>;
     </div>
   );
 }
---
+++ my-app/src/component/Firstsec.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Firstsec() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Firstsec
